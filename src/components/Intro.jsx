15cf8dc9c1232faf34402040bb73ab40bbf097de
madortill <mador.till@gmail.com>
2025-09-18 10:21:15 +0300

    finish for computer

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,41 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../css/Intro.css";
 import { useNavigate } from "react-router-dom";
-
 
 function Intro() {
   const infoArr = [
     "כדי להיות מוסמכים למתן דם מלא,",
-    'מטפלים בכירים צריכים לעמוד',
-    'במספר קריטריונים:',
+    "מטפלים בכירים צריכים לעמוד",
+    "במספר קריטריונים:",
     "אחת לשנה",
     "מעבר של הלומדה",
     "מעבר מבחן בקמפוס הדיגיטלי ",
     "אחת לשירות",
     "תחקיר מנת דם פגת תוקף",
   ];
+
+  const [hideClass, setHideClass] = useState(true);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setHideClass(false);
+    }, 15500); // 11.2 sec
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="intro">
       <div className="intro-container">
         <p className="header-intro">דגשים ללומדה</p>
         <div className="the-type-writer type-writer">
-        {infoArr.map((text, index) => (
-          <p
-            key={index}
-            style={{
-              "--delay": `${index * 2}s`,
-              "--width": `${text.length}ch`,
-            }}
-            className={index === 3 || index === 6 ? 'sub-title' : ''}
-          >
-            {text}
-          </p>
-        ))}
+          {infoArr.map((text, index) => (
+            <p
+              key={index}
+              style={{
+                "--delay": `${index * 2}s`,
+                "--width": `${text.length}ch`,
+              }}
+              className={index === 3 || index === 6 ? "sub-title" : ""}
+            >
+              {text}
+            </p>
+          ))}
+        </div>
       </div>
+
+      <div
+        className={`btn grow-rotate ${hideClass ? "hide" : ""}`}
+        onClick={() => navigate("/InfoScreen")}
+      >
+        !הבנתי
       </div>
-     
-      <div className="btn grow-rotate"  onClick={() => navigate("/InfoScreen")}>!הבנתי</div>
     </div>
   );
 }
